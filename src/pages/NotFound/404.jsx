2ebbef0fc0b886beacc90657f2befed39e1316c0
Luiz Henrique <luizhf42@gmail.com>
2022-02-03 17:12:11 -0300

    the same but for 404 page

--- src/pages/NotFound/404.jsx
+++ src/pages/NotFound/404.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 import "./404.scss";
+import notfound from "../../assets/notfound.png";
 
 export default function NotFound() {
   return (
     <div className="app">
-      <img src="src/assets/notfound.png" className="notfound-img" alt />
+      <img src={notfound} className="notfound-img" alt />
       <h1>Página não encontrada!</h1>
       <Link to="/" className="nav-btn">
         Voltar para a Home
